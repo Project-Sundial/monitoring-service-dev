0c94c8a924567fbaa5d1da6ac75ca57c8f0d6196
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2023-10-15 15:33:47 -0400

    add endpoints to add a monitor and to get all monitors, plus install nanoid for UUIDs

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,6 @@
 import express from 'express';
 const router = express.Router();
+import { nanoid } from 'nanoid';
 
 router.get('/', (_, res) => {
   const message = "Welcome to Sundial! The world's greatest "
@@ -13,6 +14,28 @@ router.post('/api/endpoint/:id', (req, res) => {
   console.log(body);
 
   res.status(200).end();
+});
+
+router.get('/api/monitors', async(req, res) => {
+  try {
+    const monitors = await getMonitors();
+    res.send(monitors);
+  } catch (e) {
+    res.send(500).send('unable to get all monitors');
+  }
+});
+
+router.post('/api/monitors', (req, res) => {
+  const body = req.body; 
+  const id = nanoid(10);
+
+  try {
+    createMonitor(body, id);
+    const wrapperStr = createWrapper(id);
+    res.send(wrapperStr);
+  } catch(e) {
+    res.send(500).send('unable to create monitor');
+  }
 });
 
 export default router;
