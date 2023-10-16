3da440273ba9294945e12c9d95abfcfea1a87ec5
Jacob Clark <jacobclark1198@gmail.com>
2023-10-16 18:46:46 +0100

    eslint config updates - also minor refactoring in other files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/api.js';
-import home from './routes/home.js'
+import home from './routes/home.js';
 
 const app = express();
 
@@ -10,9 +10,5 @@ app.use(express.json());
 
 app.use('/', home);
 app.use('/api', router);
-
-
-
-
 
 export default app;
--- src/routes/home.js
+++ src/routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 export default router.get('/', (_, res) => {
   const message = "Welcome to Sundial! The world's greatest "
-    + "open-source cron job monitoring solution.";
-  
+    + 'open-source cron job monitoring solution.';
+
   res.send(message);
 });
