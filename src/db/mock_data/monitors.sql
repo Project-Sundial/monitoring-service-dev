657288aac426d2205f2608feb1469ede884d3d4b
davidwperez@gmail.com <davidwperez@gmail.com>
2023-10-17 20:40:48 -0400

    Update to use next_alert for notification system instead of next_expected_at, verified functionality
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: marymcdonald <10123446+marymcdonald@users.noreply.github.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- src/db/mock_data/monitors.sql
+++ src/db/mock_data/monitors.sql
@@ -1,2 +1,2 @@
 INSERT INTO monitor (endpoint_key, name, schedule, command, next_expected_at)
-VALUES ('abcde', 'First monitor', '* * * * *', 'node index.js', CURRENT_TIMESTAMP);
+VALUES ('abcde', 'First monitor', '* * 3 * *', 'node index.js', CURRENT_TIMESTAMP);
--- src/db/queries.js
+++ src/db/queries.js
@@ -4,31 +4,33 @@ import parser from 'cron-parser';
 
 const getOverdue = async () => {
   const GET_OVERDUE = 'SELECT * FROM monitor WHERE '
-    + 'next_expected_at < $1';
+    + 'next_alert < $1';
   const result = await executeQuery(GET_OVERDUE, new Date());
 
   return result;
 };
 
-const dbUpdateNextExpected = async (endpoint_key) => {
-  const schedule = await dbQuery(
-    `SELECT schedule 
+const dbUpdateNextAlert = async (endpoint_key) => {
+  const results = await dbQuery(
+    `SELECT *
     FROM monitor
-    WHERE endpoint_key = ${endpoint_key}
-  `);
+    WHERE endpoint_key = $1;
+  `, [endpoint_key]);
 
-  const nextExpectedAt = parser.parseExpression(
-    schedule,
+  const target = results.rows[0];
+
+  const nextAlert = parser.parseExpression(
+    target.schedule,
     { currentDate: new Date() }
-  );
+  ).next()._date.ts +
+    target.grace_period * 1000;
 
   return dbQuery(`
     UPDATE monitor
-    SET next_expected_at = $2
+    SET next_alert = (to_timestamp($2 / 1000.0))
     WHERE endpoint_key = $1;`,
-  [endpoint_key, nextExpectedAt]);
+  [endpoint_key, nextAlert]);
 };
-
 
 const dbGetAllMonitors = () => {
   return dbQuery('SELECT * FROM monitor');
@@ -38,13 +40,13 @@ const dbAddMonitor = (params) => {
   return dbQuery(`
     INSERT INTO monitor (endpoint_key, schedule)
     VALUES ($1, $2)
-    RETURNING *;`, 
+    RETURNING *;`,
     params);
 };
 
 export {
   dbGetAllMonitors,
   dbAddMonitor,
-  dbUpdateNextExpected,
+  dbUpdateNextAlert,
   getOverdue
 };
