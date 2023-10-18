1d22b4d7e94105134b95f9a5ab014887dcf690ed
Jacob Clark <jacobclark1198@gmail.com>
2023-10-18 13:29:37 +0100

    forgotten dbAddPing line in ping controller

--- src/controllers/ping.js
+++ src/controllers/ping.js
@@ -1,9 +1,16 @@
-import { dbGetMonitorByEndpointKey, dbMonitorRecovery, dbUpdateNextAlert } from '../db/queries.js';
+import {
+  dbGetMonitorByEndpointKey,
+  dbMonitorRecovery,
+  dbUpdateNextAlert,
+  dbAddPing,
+} from '../db/queries.js';
 
 const addPing = async (req, res) => {
   try {
     const endpoint_key = req.params.endpoint_key;
     const monitor = await dbGetMonitorByEndpointKey(endpoint_key);
+
+    dbAddPing(monitor.id);
 
     if (monitor.failing) {
       dbMonitorRecovery(monitor.id);
@@ -11,8 +18,8 @@ const addPing = async (req, res) => {
 
     dbUpdateNextAlert(endpoint_key);
     res.status(200).send();
-  } catch(e) {
-    res.status(500).send('something went wrong');
+  } catch(error) {
+    res.status(500).send(error);
   }
 };
 
