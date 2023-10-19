4b2b9153297cf393ff994d5c3ea3a0f8c16b2b80
Jacob Clark <102357760+Jacob-Clark-809@users.noreply.github.com>
2023-10-19 19:29:15 +0100

    Update errorHandler.js - change log text

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -8,7 +8,7 @@ export const errorResponder = (error, req, res, next) => {
 
   let status;
   if (!error.statusCode) {
-    console.log('UNKNOWN: ', error);
+    console.log('Internal Server Error: ', error);
     status = 500;
     error.message = 'Internal Server Error';
   } else {
