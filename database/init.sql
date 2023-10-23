55760c07f8b3dbaed616ea75bc15a484f247d2df
Sofia Alere <81483266+sofalere@users.noreply.github.com>
2023-10-23 12:14:00 -0500

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -15,21 +15,6 @@ CREATE TABLE monitor (
   PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS ping;
-
-CREATE TYPE events AS ENUM ('started', 'completed');
-
-CREATE TABLE ping (
-  id serial,
-  -- run_id integer,
-  monitor_id integer,
-  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  -- event events NOT NULL,
-  PRIMARY KEY (id),
-  -- FOREIGN KEY (run_id) REFERENCES run(id) ON DELETE CASCADE
-  FOREIGN KEY (monitor_id) REFERENCES monitor (id) ON DELETE CASCADE
-);
-
 DROP TABLE IF EXISTS run;
 
 CREATE TYPE states AS ENUM ('started', 'completed', 'failed');
