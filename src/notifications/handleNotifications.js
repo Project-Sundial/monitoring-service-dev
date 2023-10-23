46c7317fe2dee2576b5fe1fa47705c4b41731e14
Jacob Clark <jacobclark1198@gmail.com>
2023-10-23 16:37:39 +0100

    restructuring

---
+++ src/notifications/handleNotifications.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { dbGetOverdue, dbUpdateFailingMonitors } from '../db/queries.js';
+
+(async () => {
+  try {
+    const overdueMonitors = await dbGetOverdue();
+    console.log(overdueMonitors);
+    await dbUpdateFailingMonitors(overdueMonitors.map(({ id }) => id));
+  } catch (error) {
+    console.log(error);
+  }
+})();
