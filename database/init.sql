27a834864d73fa0f9b300d400873774be164a1da
sofalere <sofalere@gmail.com>
2023-10-23 11:34:18 -0500

    File structure changes

---
+++ database/init.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS monitor;
+
+CREATE TABLE monitor (
+  id serial,
+  endpoint_key text UNIQUE NOT NULL,
+  name text,
+  schedule text NOT NULL,
+  command text,
+  active boolean NOT NULL DEFAULT true,
+  failing boolean NOT NULL DEFAULT false,
+  next_alert timestamp,
+  realert_interval integer DEFAULT 480, -- mins
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+  grace_period int NOT NULL DEFAULT 30, -- TESTING PURPOSES ONLY
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS ping;
+
+CREATE TYPE events AS ENUM ('started', 'completed');
+
+CREATE TABLE ping (
+  id serial,
+  -- run_id integer,
+  monitor_id integer,
+  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  -- event events NOT NULL,
+  PRIMARY KEY (id),
+  -- FOREIGN KEY (run_id) REFERENCES run(id) ON DELETE CASCADE
+  FOREIGN KEY (monitor_id) REFERENCES monitor (id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS run;
+
+CREATE TYPE states AS ENUM ('started', 'completed', 'failed');
+
+CREATE TABLE run (
+  id serial,
+  monitor_id integer NOT NULL,
+  start_time integer NOT NULL,
+  duration integer,
+  state states NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (monitor_id) REFERENCES monitor(id) ON DELETE CASCADE
+);
