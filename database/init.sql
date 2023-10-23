a8256550a961a1682a956ed3883cbe1bf439fb98
Sofia Alere <81483266+sofalere@users.noreply.github.com>
2023-10-23 12:12:52 -0500

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -36,20 +36,6 @@ CREATE TYPE states AS ENUM ('started', 'completed', 'failed');
 
 CREATE TABLE run (
   id serial,
-  monitor_id integer NOT NULL,
-  start_time integer NOT NULL,
-  duration integer,
-  state states NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (monitor_id) REFERENCES monitor(id) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS run;
-
-CREATE TYPE states AS ENUM ('started', 'completed', 'failed');
-
-CREATE TABLE run (
-  id serial,
   run_token text NOT NULL,
   monitor_id integer NOT NULL,
   start_time integer NOT NULL,
