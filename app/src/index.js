e51c41781cde9596001a06269cbdb68b54cccb35
Jacob Clark <jacobclark1198@gmail.com>
2023-10-26 12:39:33 +0100

    remove missed ping provider - init in app.listen
    Co-authored-by: davidscoding <80804607+davidscoding@users.noreply.github.com>
    Co-authored-by: Mary McDonald <mcdonald.e.mary@gmail.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,15 @@
 import 'dotenv/config';
 import app from './app.js';
 import setupJobs from './utils/setupNotifications.js';
-import missedPingsMqProvider from './db/missedPingsMqProvider.js';
+import MissedPingsMq from './db/MissedPingsMq.js';
 
 const port = process.env.PORT;
 app.listen(port, async () => {
   console.log(`Sundial app server listening on port ${port}`);
   setupJobs();
+  await MissedPingsMq.init();
 
   // MQ tests please delete
-  const MissedPingsMq = await missedPingsMqProvider();
   MissedPingsMq.addStartJob({ monitorId: 12 }, 5);
   MissedPingsMq.addEndJob({ monitorId: 15 }, 10);
   MissedPingsMq.addSoloJob({ monitorId: 20 }, 15);
