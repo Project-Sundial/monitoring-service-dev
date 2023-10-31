df012587813c8b8f5e322135bf61428d665e1cce
davidwperez@gmail.com <davidwperez@gmail.com>
2023-10-31 14:58:47 -0400

    Changes routes to match current 'sync' paradigm
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: marymcdonald <10123446+marymcdonald@users.noreply.github.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- app/src/controllers/scheduler.js
+++ app/src/controllers/scheduler.js
@@ -1,7 +1,105 @@
 import { nanoid } from 'nanoid';
 import axios from 'axios';
+import { nanoid } from 'nanoid';
+import { dbGetAllMonitors, dbGetRunsByMonitorId, dbAddMonitor, dbDeleteMonitor } from '../db/queries.js';
 
-let cache = [{command: 'test', schedule: '* * * * *', endpointKey: '334534t'}];
+// workflow:
+
+// in app route:
+
+// queueSync
+// filter item
+// IF the task is a delete, add or if the update is changing the schedule or command do below
+//         app creates endpointKey for new jobs
+//         store in "unsyncedUpdates" variable
+//         ping daemon
+//     ELSE
+//         refresh UI
+//     ALWAYS updateDB
+
+// set variable
+
+// 3 helpers
+//     add monitors iterate through and add
+//     delete iterate through and delete
+//     update iterate through and delete
+
+// ping daemon
+
+
+// sync
+// respond with variable
+
+const stagedChanges = {
+  add: [],
+  delete: [],
+  update: [],
+};
+
+const stageChanges = async (req, res, next) => {
+  try {
+    const { ...jobData } = req.body;
+    const endpointKey = nanoid(10);
+
+    const newJobData = {
+      endpointKey,
+      ...jobData
+    };
+
+    cache.push(newJobData);
+    console.log('cache data:', cache);
+
+    //hard coded for now
+    await axios.post('http://host.docker.internal:56789/trigger-sync');
+    console.log('Pinging the http server');
+    res.send(200);
+  } catch (error) {
+    next(error);
+  }
+  
+  try {
+    console.log('hi');
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getChanges = async (req, res, next) => {
+  try {
+    res.send(stagedChanges);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export {
+  stageChanges,
+  getChanges
+};
+
+
+// workflow:
+
+// in app route:
+
+// queueSync
+// filter item
+// IF the task is a delete, add or if the update is changing the schedule or command do below
+//         app creates endpointKey for new jobs
+//         store in "unsyncedUpdates" variable
+//         ping daemon
+//     ELSE
+//         refresh UI
+//     ALWAYS updateDB
+
+// set variable
+
+// 3 helpers
+//     add monitors iterate through and add
+//     delete iterate through and delete
+//     update iterate through and delete
+
+// ping daemon
 
 const addJob = async (req, res, next) => {
   try {
--- app/src/routes/api.js
+++ app/src/routes/api.js
@@ -2,21 +2,17 @@ import express from 'express';
 const router = express.Router();
 import { getMonitors, getMonitorRuns, addMonitor, deleteMonitor } from '../controllers/monitor.js';
 import { addPing } from '../controllers/ping.js';
-import { queueSync, sync } from '../controllers/sync.js';
-import { getUpdates, addJob, syncUpdates } from '../controllers/scheduler.js';
+import { stageChanges, getChanges } from '../controllers/scheduler.js';
 
 router.get('/monitors', getMonitors);
 router.get('/monitors/:id', getMonitorRuns);
 router.post('/monitors', addMonitor);
 router.delete('/monitors/:id', deleteMonitor);
 
-router.post('/scheduler-sync', queueSync);
-router.get('/scheduler-sync', sync);
+router.post('/scheduler/staged', stageChanges);
+router.get('/scheduler/staged', getChanges);
+// router.post('/scheduler/sync', syncChanges);
 
 router.post('/pings/:endpoint_key', addPing);
-
-router.post('/scheduler/add', addJob);
-router.get('/scheduler/updates', getUpdates);
-// router.post('/synced', syncUpdates);
 
 export default router;
