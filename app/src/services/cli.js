57c509ada284e85e0369968f008a8b04720957f5
davidwperez@gmail.com <davidwperez@gmail.com>
2023-11-01 13:09:04 -0400

    Fixes import typo bug
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: marymcdonald <10123446+marymcdonald@users.noreply.github.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- app/src/services/cli.js
+++ app/src/services/cli.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {
   LOCAL_HOST
-} from '../constants/routes';
+} from '../constants/routes.js';
 
 export const triggerSync = async () => {
   await axios.post(LOCAL_HOST);
