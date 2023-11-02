62ce33ed34a74f602934b24ea2027fa22dc368f0
Jacob Clark <jacobclark1198@gmail.com>
2023-11-02 23:08:49 +0000

    added authenticator to sse
    Co-authored-by: davidscoding <80804607+davidscoding@users.noreply.github.com>
    Co-authored-by: Mary McDonald <mcdonald.e.mary@gmail.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- app/src/app.js
+++ app/src/app.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/monitors', authenticator);
 app.use('/api/pings', authenticator);
+app.use('/sse', authenticator);
 
 app.use('/', home);
 app.use('/api', api);
