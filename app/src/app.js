c6ad07ede2aa03850750016d79cd2d86590cad63
Jacob Clark <102357760+Jacob-Clark-809@users.noreply.github.com>
2023-11-03 20:19:52 +0000

    Update app.js

--- app/src/app.js
+++ app/src/app.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/monitors', authenticator);
+app.use('/api/pings', authenticator);
 
 app.use('/', home);
 app.use('/api', api);
