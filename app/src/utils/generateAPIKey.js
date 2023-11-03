074011fbb7cecea273c7dc2ca081b00aaf7ca9c7
Mary McDonald <mcdonald.e.mary@gmail.com>
2023-11-03 13:41:48 -0400

    Create generate API key method
    Co-authored-by: David Perez <davidwperez@gmail.com>
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: Sofia Alere <sofalere@gmail.com>

---
+++ app/src/utils/generateAPIKey.js
@@ -0,0 +1,8 @@
+import nanoid from 'nanoid';
+
+const generateAPIKey = () => {
+    const key = 'pfx_' + nanoid(); //pfx indicates that it's a key; default nanoid is 21 chars long
+    return key;
+}
+
+export default generateAPIKey;
