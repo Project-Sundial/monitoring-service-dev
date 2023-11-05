115a05c15fd3565266b4eace8e8124955227b59a
Mary McDonald <mcdonald.e.mary@gmail.com>
2023-11-04 22:18:03 -0400

    Adding indenting
    Co-authored-by: David Perez <davidwperez@gmail.com>
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: Sofia Alere <sofalere@gmail.com>

--- ui/src/components/APIKeyList.jsx
+++ ui/src/components/APIKeyList.jsx
@@ -7,10 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
-function createData(prefix, name, created_at) {
-    return { prefix, name, created_at };
-  }
+import { CONTAINER_COLOR } from '../constants/colors';
 
 const APIKeyList = (onError) => {
     const [keys, setKeys] = useState([]);
@@ -28,32 +25,43 @@ const APIKeyList = (onError) => {
         fetchList();
     }, []);
 
+    const divStyle = {
+        boxShadow: '0 1px 3px rgba(0,0,0,0.12)',
+        backgroundColor: CONTAINER_COLOR,
+        borderRadius: '8px',
+        maxWidth: '90%'
+      }
+
     return (
-        <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-            <TableRow>
-                <TableCell>API Key Prefix</TableCell>
-                <TableCell align="right">Name</TableCell>
-                <TableCell align="right">Created on</TableCell>
-            </TableRow>
-            </TableHead>
-            <TableBody>
-            {keys.map((key) => (
-                <TableRow
-                key={key.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                <TableCell component="th" scope="row">
-                    {key.prefix}
-                </TableCell>
-                <TableCell align="right">{key.name}</TableCell>
-                <TableCell align="right">{key.created_at}</TableCell>
-                </TableRow>
-            ))}
-            </TableBody>
-        </Table>
-        </TableContainer>
+        <div style={{ marginTop: '5%', marginLeft: '5%'}}>
+            <div style={divStyle}>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>API Key Prefix</TableCell>
+                                <TableCell align="right">Name</TableCell>
+                                <TableCell align="right">Created on</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {keys.map((key) => (
+                            <TableRow
+                            key={key.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                            <TableCell component="th" scope="row">
+                                {key.prefix}
+                            </TableCell>
+                            <TableCell align="right">{key.name}</TableCell>
+                            <TableCell align="right">{key.created_at}</TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        </div>
     );
 
 };
