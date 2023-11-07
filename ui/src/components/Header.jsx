6cf69ba76b4a20a1be3bfcee6706408b4bf55d67
Jacob Clark <jacobclark1198@gmail.com>
2023-11-07 20:39:40 +0000

    added retractable drawer
    Co-authored-by: davidscoding <80804607+davidscoding@users.noreply.github.com>
    Co-authored-by: Mary McDonald <mcdonald.e.mary@gmail.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -1,9 +1,17 @@
-import { Box, AppBar, Toolbar, Typography } from '@mui/material';
+import { Box, AppBar, Toolbar, Typography, Drawer } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import IconButton from '@mui/material/IconButton';
 import { THEME_COLOR } from '../constants/colors';
+import { useState } from 'react';
+import DrawerList from './DrawerList';
 
 const Header = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(drawerOpen => !drawerOpen);
+  };
+
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -15,9 +23,17 @@ const Header = () => {
               color="inherit"
               aria-label="open drawer"
               sx={{ mr: 2 }}
+              onClick={toggleDrawer}
             >
               <MenuIcon />
             </IconButton>
+            <Drawer
+              anchor="left"
+              open={drawerOpen}
+              onClose={toggleDrawer}  
+            >
+              <DrawerList toggleDrawer={toggleDrawer} />
+            </Drawer>
             <Typography
               variant="h6"
               noWrap
