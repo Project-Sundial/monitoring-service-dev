18bda4fb7a793ef351635171493d008a7823a2ad
sofalere <sofalere@gmail.com>
2023-11-06 20:48:41 -0600

    Base url var changed

--- app/src/constants/routes.js
+++ app/src/constants/routes.js
@@ -2,3 +2,6 @@ export const BASE_URL = process.env.BASE_URL || 'http://localhost:58669';
 export const SYNC = '/api/monitors';
 export const DISCOVER = '/api/monitors/';
 export const LOCAL_HOST = 'http://host.docker.internal:56789/trigger-sync';
+
+app-both     latest    abdcbfb9aea5   15 seconds ago   203MB
+ui-both      latest    e7f054715311   2 hours ago      52.7MB
